b5ccdc3048081510e87c9c4e194ed742a0c9a5eb
verixiaapps <surfer847@outlook.com>
2026-03-08 12:50:19 -0400

    Create app-link.js

---
+++ scam-check-now/app-link.js
@@ -0,0 +1,14 @@
+function openApp(){
+
+// CURRENT DESTINATION (before App Store launch)
+window.location.href="/scam-check-now/index.html"
+
+/*
+WHEN THE APP IS APPROVED
+CHANGE THE LINE ABOVE TO:
+
+window.location.href="https://apps.apple.com/app/idXXXXXXXX"
+
+*/
+
+}
